7a5ce949a2e698fa2218dfd72e3c448bdbfc2780
BrenoIFCE <breno.pinheiro07@aluno.ifce.edu.br>
2024-08-28 20:26:57 -0300

    secao de comentarios

--- livroAPI/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-
-Route::post('/books', [BookController::class, 'store']);
-Route::post('/books/{id}/pdf', [BookController::class, 'showPdf']);
-
